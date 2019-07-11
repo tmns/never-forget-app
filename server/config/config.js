7b0650bfd0cc66159f8222f6f0c5ea413af89580
sairim <spiri@protonmail.com>
2019-07-11 12:42:13 +0300

    small change

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,6 @@ export const {
   DB_URI = `mongodb://localhost:27017/neverForgetBeDB`,
   SESS_NAME = 'sessionId',
   SESS_SECRET = 'test-secret',
-  SESS_LIFETIME = 1000 * 60 * 60 
+  SESS_LIFETIME = 1000 * 60 * 60 * 24
 } = process.env;
 
