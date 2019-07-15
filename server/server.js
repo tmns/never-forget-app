38d8bfe33f5467620276519ba18e5a48d4810147
sairim <spiri@protonmail.com>
2019-07-15 18:06:35 +0300

    remove prod express route

--- server/server.js
+++ server/server.js
@@ -74,12 +74,12 @@ async function start() {
   app.use(cors(corsOptions));
 
   // serve static assets if in prod
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-    });
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static('../client/build'));
+  //   app.get('*', (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  //   });
+  // }
 
   server.applyMiddleware({ app, cors: false });
 
