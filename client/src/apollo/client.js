7cd984ba596da2a575daf51fd043e2ec0c93b64e
sairim <spiri@protonmail.com>
2019-07-16 13:48:37 +0300

    simplify config of apollo client

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -1,10 +1,8 @@
 import ApolloClient from "apollo-boost";
 
-import * as config from '../config';
-
 const client = new ApolloClient({
-  uri: `/graphql`,
-  credentials: "include"
+    uri: `/graphql`,
+    credentials: "include"
 });
 
 export default client;
